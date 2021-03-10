2e9c85a925ecde454490475347403046380d36f4
GhostGamesV11 <47499005+GhostGamesV11@users.noreply.github.com>
2021-03-10 15:16:59 +0100

    Add files via upload

---
+++ rozdzial7/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//zad 1
+//    printf("Wpisz cokolwiek: ");
+//    char z;
+//    int spc=0,znak=0;
+//    while((z=getchar())!='#')
+//    {
+//        if(isspace(z))
+//            spc++;
+//        if(isgraph(z))
+//            znak++;
+//    }
+//    printf("Ilosc spacji i ilosc nowych lini = %d ilosc innych znakow = %d",spc,znak);
+
+//zad 2
+//    char wpis;
+//    int as;
+//    printf("Wpisz cokolwiek a zostanie wyswietlony jego kod ASCII. Zakoncz program #\n");
+//    while((wpis=getchar())!='#')
+//    {
+//      as=wpis;
+//      if(wpis == ' '||wpis=='\n')
+//        continue;
+//      else
+//      {
+//          printf(" %c po zamianie = %d ",wpis,as);
+//      }
+//
+//
+//
+//    }
+//    printf("\nProgram sie zakonczyl");
+
+//zad 3
+//    int li,pr=0,npr=0,lpr=0,lnpr=0;
+//    float spr,snpr;
+//    printf("Podaj liczby calkowite ujemnie lub dodatnie Program konczy sie po wpisaniu 0\n");
+//    while(1)
+//    {
+//       scanf("%d",&li);
+//       if(li==0)
+//            break;
+//       if(li%2==0)
+//       {
+//           pr+=li;
+//           lpr++;
+//       }
+//       else
+//       {
+//           npr+=li;
+//           lnpr++;
+//       }
+//    }
+//    if(lpr>0)
+//    {
+//        spr=(float)pr/(float)lpr;
+//        printf("Podano %d liczby parzystych ||| Suma podanych liczb wynosi %d ||| Srednia tych liczb parzystych wynosi %g\n",lpr,pr,spr);
+//    }
+//    if(lnpr>0)
+//    {
+//        snpr=(float)pr/(float)lnpr;
+//        printf("Podano %d liczby nieparzystych ||| Suma podanych liczb wynosi %d ||| Srednia tych liczb nieparzystych wynosi %g\n",lnpr,npr,snpr);
+//    }
+
+
+
+
+
+//zad 4
+//    char x;
+//    int zmiana1=0,zmiana2=0,suma=0;
+//    printf("Wpisz cokolwiek: ");
+//    while((x=getchar())!='#')
+//    {
+//        if(x=='.')
+//        {
+//            x='!';
+//            zmiana1++;
+//            printf("Nowy tekst:%c\n",x);
+//
+//        }
+//        else
+//        {
+//            if (x=='!')
+//            {
+//                x='!';
+//                zmiana2++;
+//                printf("Nowy tekst:%c%c\n",x,x);
+//            }
+//        }
+//    }
+//    suma=zmiana1+zmiana2;
+//    printf("\nLiczba zmian:%d",suma);
+
+    return 0;
+}
